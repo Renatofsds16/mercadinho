49a8a934df920f2fc58c08f4375719f27a9f585d
Renato firmino <renatofss16@gmail.com>
2025-01-10 23:02:27 -0300

    funcoes mais importante

--- mercado/cloud/main.js
+++ mercado/cloud/main.js
@@ -54,3 +54,72 @@ Parse.Cloud.define("get-category-list",async (request)=>{
 		}
 	});
 });
+
+Parse.Cloud.define("sign-up", async (request)=>{
+	if(request.params.fullName == null) throw "INVALID FULLNAME";
+	if(request.params.cpf == null) throw "INVALID CPF";
+	if(request.params.phone == null) throw "INVALID PHONE";
+
+	const user = new Parse.User();
+	user.set("username",request.params.email);
+	user.set("email",request.params.email);
+	user.set("password",request.params.password);
+	user.set("fullName",request.params.fullName);
+	user.set("cpf",request.params.cpf);
+	user.set("phone",request.params.phone);
+
+	try {
+		const resultUser = await user.signUp(null,{useMasterKey: true});
+		const userJSON = resultUser.toJSON();
+		return formaterUser(userJSON);
+	}catch (e){
+		throw "INVALID DATA";
+	}
+
+});
+
+Parse.Cloud.define("login",async (request)=>{
+	try {
+		const user = await Parse.User.logIn(request.params.email,request.params.password);
+		const userJSON = user.toJSON();
+		return formaterUser(userJSON);
+
+	}catch (e){
+		throw "INVALID CREDENTIALS";
+	}
+
+});
+
+Parse.Cloud.define("validate-token",async (request)=>{
+	try{
+		return formaterUser(request.user.toJSON());
+	}catch (e){
+		throw "INVALID TOKEN";
+	}
+});
+Parse.Cloud.define("chenge-password",async (request)=>{
+	if(request.user == null) throw "INVALID USER";
+	const user = await Parse.User.logIn(request.params.email,request.params.currentPassword);
+	if(user.id != request.user.id) throw "INVALID USER";
+	try {
+		user.set("password",request.params.password);
+		user.save(null,{useMasterKey: true});
+	}catch (e){
+		throw "ERRO";
+	}
+});
+
+function formaterUser(userJSON){
+	return {
+		id: userJSON.objectId,
+		name: userJSON.fullName,
+		email: userJSON.email,
+		phone: userJSON.phone,
+		cpf: userJSON.cpf,
+		tokem:userJSON.sessionToken
+		
+	};
+}
+
+
+
